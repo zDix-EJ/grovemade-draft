b3dbbfb8520028eac7027945cc16ae125dcdcb3c
Ilya <ilya_gladkikh.program@mail.ru>
2025-06-07 01:53:26 +0300

    Animation added and fixes

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+/* Плавное перемещение по якорным ссылкам */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth',
+		})
+	})
+})
